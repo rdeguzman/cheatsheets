5a2e5c5dcb6c8efc8b7c7a325100ab02bc5c81a7
rdeguzman <rupertdguzman@gmail.com>
2018-03-18 14:10:37 +1100

    Add osx bootable and rails

---
+++ rails.md
@@ -0,0 +1,8 @@
+---
+layout: default
+---
+Rails CheatSheet
+---
+
+### Commands
+	rails new myapp --database=postgresql --skip-bundle --skip-coffee --skip-test --webpack
---
+++ scripts/create_osx_bootable.sh
@@ -0,0 +1,2 @@
+cd "/Applications/Install OS X El Capitan.app/Contents/Resources/"
+sudo ./createinstallmedia --volume /Volumes/OSX --applicationpath "/Applications/Install OS X El Capitan.app" --nointeraction
